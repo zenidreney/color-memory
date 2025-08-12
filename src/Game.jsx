755c0a57aaa3b336952216a22b6e68f87845ad45
Deniz <auo@tuta.io>
2025-08-12 11:00:30 +0200

    initilized the draft of color memory game from react-state repository

---
+++ src/Game.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import Color from "./Color";
+import { nanoid } from "nanoid";
+
+console.log(nanoid())
+
+const baseColors = ["blue", "red", "black", "white", "green", "pink", "darkviolet", "khaki"];
+
+function Game() {
+    const [colorsArray, setColorsArray] = useState(() =>
+        baseColors.map((color) => {
+            return {
+                id: nanoid(),
+                colorValue: color,
+                isHeld: false
+            };
+        })
+    );
+
+    //console.log(colorsArray)
+
+    const colorBtns = colorsArray.map((color) => (
+        <Color
+            key={color.id}
+            id={color.id}
+            color={color.colorValue}
+            isHeld={color.isHeld}
+            hold={() => holdColor(color.id)}
+        />
+    ));
+
+    function mixColors() {
+        const mixedColors = colorsArray.map((color) => {
+            const rI = Math.floor(Math.random() * baseColors.length);
+            const randomColor = baseColors[rI];
+
+            if (color.isHeld === false) {
+                return {
+                    ...color,
+                    colorValue: randomColor,
+                };
+            } else {
+                return color
+            }
+        });
+
+        setColorsArray(mixedColors);
+    }
+
+    function holdColor(id) {
+        setColorsArray((prev) =>
+            prev.map(function (color) {
+                return color.id === id
+                    ? {
+                          ...color,
+                          isHeld: true
+                      }
+                    : color;
+            })
+        );
+    }
+
+    return (
+        <article>
+            <h2>Choose the same colors</h2>
+
+            <div className="color-container">{colorBtns}</div>
+
+            <button className="mix-button" onClick={mixColors}>
+                Mix Colors
+            </button>
+        </article>
+    );
+}
+
+export default Game;
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+    
+        <header>
+            
+            <h1>Color-Memory Game</h1>
+            
+        </header>
+        
+    )
+}
+
+export default Header
